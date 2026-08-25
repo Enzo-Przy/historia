98d3be095ce4e1e3cdec3534d176dc2327e1f4e6
Enzo-Przy <enzo.przybylski@escola.pr.gov.br>
2026-08-25 12:13:32 -0300

    h

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ function mostraAlternativa(){
 
     for (const alternativa of perguntaAtual.alternativas){
 
-        const botaoAlternativas = document.creatElement("button")
+        const botaoAlternativas = document.createElement("button")
         botaoAlternativas.textContent = alternativa.texto 
         botaoAlternativas.addEventListener ("click", ()=> respostaSelecionada(alternativa))
         caixaAlternativas.appendChild(botaoAlternativas)
